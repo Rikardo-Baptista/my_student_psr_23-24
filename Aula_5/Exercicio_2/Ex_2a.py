11f523107d124e6155a252f09b707674d8693034
Rikardo-Baptista <ricardo.baptista@ua.pt>
2023-10-12 11:49:36 +0100

    update

--- Aula_5/Exercicio_2/Ex_2a.py
+++ Aula_5/Exercicio_2/Ex_2a.py
@@ -2,6 +2,7 @@
 # Shebang line
 
 import cv2
+import argparse
 
 def main():
 
@@ -11,11 +12,11 @@ def main():
     print(vars)
     exit(0)
 
-    image_filename = '/atlascar2_multichannel_thresholded.png'
-    image = cv2.imread(image_filename, cv2.IMREAD_COLOR) # Load an image
+    #image_filename = '/atlascar2_multichannel_thresholded.png'
+    #image = cv2.imread(image_filename, cv2.IMREAD_COLOR) # Load an image
 
-    cv2.imshow('window', image)  # Display the image
-    cv2.waitKey(0) # wait for a key press before proceeding
+    #cv2.imshow('window', image)  # Display the image
+    #cv2.waitKey(0) # wait for a key press before proceeding
 
 
 if __name__ == '__main__':
